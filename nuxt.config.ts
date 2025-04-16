c33c11412c02ebfdc3443d99029e8a76b4ebde80
ArnarValur <arnarvalurjonsson@gmail.com>
2025-04-16 14:30:53 +0200

    Enhance Nuxt configuration by adding Tailwind CSS, ESLint module, and Nuxt Devtools. Update package dependencies accordingly.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', 'reka-ui/nuxt'],
+  modules: [
+    '@pinia/nuxt',
+    'reka-ui/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@nuxt/devtools',
+  ],
 })
